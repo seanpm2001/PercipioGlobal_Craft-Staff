de1182acb44113c0ae7800ff57744ed1e7e3fe9f
Stefanie Gevaert <>
2022-02-07 10:55:43 +0100

    feat: added AutoEnrolmentAssesment active record

---
+++ src/records/AutoEnrolmentAssesment.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace percipiolondon\staff\records;
+
+use craft\db\ActiveRecord;
+use percipiolondon\staff\db\Table;
+
+/**
+ * Addresses record
+ * @property \DateTime assessmentDate;
+ * @property string employeeState;
+ * @property integer age;
+ * @property string ukWorker;
+ * @property string payPeriod;
+ * @property integer ordinal;
+ * @property double earningsInPeriod;
+ * @property double qualifyingEarningsInPeriod;
+ * @property string aeExclusionCode;
+ * @property string status;
+ * @property string reason;
+ * @property integer action;
+ * @property integer employee;
+ * @property string assessmentId;
+ */
+
+class AutoEnrolmentAssesment extends ActiveRecord
+{
+    // Public Static Methods
+    // =========================================================================
+
+    /**
+     * Declares the name of the database table associated with this AR class.
+     * By default this method returns the class name as the table name by calling [[Inflector::camel2id()]]
+     * with prefix [[Connection::tablePrefix]]. For example if [[Connection::tablePrefix]] is `tbl_`,
+     * `Customer` becomes `tbl_customer`, and `OrderItem` becomes `tbl_order_item`. You may override this method
+     * if the table is not named after this convention.
+     *
+     * By convention, tables created by plugins should be prefixed with the plugin
+     * name and an underscore.
+     *
+     * @return string the table name
+     */
+    public static function tableName()
+    {
+        return Table::AUTO_ENROLMENT_ASSESSMENT;
+    }
+}
