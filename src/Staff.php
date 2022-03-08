2532b5e70cb91f2c641124cd00a02e379daca22c
Stefanie Gevaert <>
2022-03-08 18:22:04 +0100

    refactor: removed the unused services declerations

--- src/Staff.php
+++ src/Staff.php
@@ -29,12 +29,6 @@ use craft\web\twig\variables\CraftVariable;
 
 use nystudio107\pluginvite\services\VitePluginService;
 
-use percipiolondon\staff\services\Employers as EmployersService;
-use percipiolondon\staff\services\Employees as EmployeesService;
-use percipiolondon\staff\services\PayRun as PayRunService;
-use percipiolondon\staff\services\PayRunEntries as PayRunEntriesService;
-use percipiolondon\staff\services\Pensions as PensionsService;
-
 use percipiolondon\staff\assetbundles\staff\StaffAsset;
 use percipiolondon\staff\models\Settings;
 use percipiolondon\staff\elements\Employer as EmployerElement;
@@ -69,11 +63,6 @@ use yii\base\Event;
  * @package   Staff
  * @since     0.2.0
  *
- * @property  EmployersService      $employers
- * @property  EmployeesService      $employees
- * @property  PayRunService         $payRun
- * @property  PayRunEntriesService  $payRunEntries
- * @property  PensionsService       $pensions
  * @property  Settings              $settings
  * @property  VitePluginService     $vite
  * @method    Settings              getSettings()
