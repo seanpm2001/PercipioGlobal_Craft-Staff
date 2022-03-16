6b5b0ec3dabb4c114a72eae4c2b6816ceac45cbb
Michael Thomas <michael@dadamotion.be>
2022-03-16 13:31:24 +0100

    refactor(employee): cleanup

--- src/elements/Employee.php
+++ src/elements/Employee.php
@@ -11,68 +11,21 @@
 namespace percipiolondon\staff\elements;
 
 use craft\db\Query;
-use percipiolondon\staff\helpers\Logger;
-use percipiolondon\staff\helpers\Security as SecurityHelper;
-use percipiolondon\staff\records\EmploymentDetails;
-use percipiolondon\staff\records\LeaverDetails;
-use percipiolondon\staff\records\PersonalDetails;
-use percipiolondon\staff\Staff;
 
 use Craft;
-use craft\elements\User;
 use craft\base\Element;
 use craft\elements\db\ElementQuery;
 use craft\elements\db\ElementQueryInterface;
+use craft\elements\User;
+
 use percipiolondon\staff\elements\db\EmployeeQuery;
-use percipiolondon\staff\records\Employee as EmployeeRecord;
-use percipiolondon\staff\records\Permission;
-use percipiolondon\staff\records\Employer;
+
 use yii\db\Exception;
 
 /**
  * Employee Element
- *
- * Element is the base class for classes representing elements in terms of objects.
- *
- * @property FieldLayout|null      $fieldLayout           The field layout used by this element
- * @property array                 $htmlAttributes        Any attributes that should be included in the element’s DOM representation in the Control Panel
- * @property int[]                 $supportedSiteIds      The site IDs this element is available in
- * @property string|null           $uriFormat             The URI format used to generate this element’s URL
- * @property string|null           $url                   The element’s full URL
- * @property \Twig_Markup|null     $link                  An anchor pre-filled with this element’s URL and title
- * @property string|null           $ref                   The reference string to this element
- * @property string                $indexHtml             The element index HTML
- * @property bool                  $isEditable            Whether the current user can edit the element
- * @property string|null           $cpEditUrl             The element’s CP edit URL
- * @property string|null           $thumbUrl              The URL to the element’s thumbnail, if there is one
- * @property string|null           $iconUrl               The URL to the element’s icon image, if there is one
- * @property string|null           $status                The element’s status
- * @property Element               $next                  The next element relative to this one, from a given set of criteria
- * @property Element               $prev                  The previous element relative to this one, from a given set of criteria
- * @property Element               $parent                The element’s parent
- * @property mixed                 $route                 The route that should be used when the element’s URI is requested
- * @property int|null              $structureId           The ID of the structure that the element is associated with, if any
- * @property ElementQueryInterface $ancestors             The element’s ancestors
- * @property ElementQueryInterface $descendants           The element’s descendants
- * @property ElementQueryInterface $children              The element’s children
- * @property ElementQueryInterface $siblings              All of the element’s siblings
- * @property Element               $prevSibling           The element’s previous sibling
- * @property Element               $nextSibling           The element’s next sibling
- * @property bool                  $hasDescendants        Whether the element has descendants
- * @property int                   $totalDescendants      The total number of descendants that the element has
- * @property string                $title                 The element’s title
- * @property string|null           $serializedFieldValues Array of the element’s serialized custom field values, indexed by their handles
- * @property array                 $fieldParamNamespace   The namespace used by custom field params on the request
- * @property string                $contentTable          The name of the table this element’s content is stored in
- * @property string                $fieldColumnPrefix     The field column prefix this element’s content uses
- * @property string                $fieldContext          The field context this element’s content uses
- *
- * http://pixelandtonic.com/blog/craft-element-types
- *
- * @author    Percipio
- * @package   Staff
- * @since     1.0.0-alpha.1
  */
+
 class Employee extends Element
 {
     // Public Properties
@@ -139,119 +92,11 @@ class Employee extends Element
     }
 
     /**
-     * Returns whether elements of this type will be storing any data in the `content`
-     * table (tiles or custom fields).
-     *
-     * @return bool Whether elements of this type will be storing any data in the `content` table.
-     */
-    public static function hasContent(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Returns whether elements of this type have traditional titles.
-     *
-     * @return bool Whether elements of this type have traditional titles.
-     */
-    public static function hasTitles(): bool
-    {
-        return false;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public static function hasUris(): bool
-    {
-        return false;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public static function hasStatuses(): bool
-    {
-        return true;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getStatus()
-    {
-        $status = parent::getStatus();
-
-        return $status;
-    }
-
-    /**
-     * Returns whether elements of this type have statuses.
-     *
-     * If this returns `true`, the element index template will show a Status menu
-     * by default, and your elements will get status indicator icons next to them.
-     *
-     * Use [[statuses()]] to customize which statuses the elements might have.
-     *
-     * @return bool Whether elements of this type have statuses.
-     * @see statuses()
-     */
-    public static function statuses(): array
-    {
-        return [
-            self::STATUS_ENABLED => Craft::t('staff-management', 'Enabled'),
-            self::STATUS_DISABLED => Craft::t('staff-management', 'Disabled'),
-        ];
-    }
-
-    public static function isLocalized(): bool
-    {
-        return true;
-    }
-
-    /**
      * Creates an [[ElementQueryInterface]] instance for query purpose.
      *
      * The returned [[ElementQueryInterface]] instance can be further customized by calling
      * methods defined in [[ElementQueryInterface]] before `one()` or `all()` is called to return
      * populated [[ElementInterface]] instances. For example,
-     *
-     * ```php
-     * // Find the entry whose ID is 5
-     * $entry = Entry::find()->id(5)->one();
-     *
-     * // Find all assets and order them by their filename:
-     * $assets = Asset::find()
-     *     ->orderBy('filename')
-     *     ->all();
-     * ```
-     *
-     * If you want to define custom criteria parameters for your elements, you can do so by overriding
-     * this method and returning a custom query class. For example,
-     *
-     * ```php
-     * class Product extends Element
-     * {
-     *     public static function find()
-     *     {
-     *         // use ProductQuery instead of the default ElementQuery
-     *         return new ProductQuery(get_called_class());
-     *     }
-     * }
-     * ```
-     *
-     * You can also set default criteria parameters on the ElementQuery if you don’t have a need for
-     * a custom query class. For example,
-     *
-     * ```php
-     * class Customer extends ActiveRecord
-     * {
-     *     public static function find()
-     *     {
-     *         return parent::find()->limit(50);
-     *     }
-     * }
-     * ```
      *
      * @return ElementQueryInterface The newly created [[ElementQueryInterface]] instance.
      */
@@ -301,24 +146,6 @@ class Employee extends Element
     }
 
     /**
-     * Returns whether the current user can edit the element.
-     *
-     * @return bool
-     */
-    public function getIsEditable(): bool
-    {
-        return true;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCpEditUrl()
-    {
-        return 'staff-management/employees/' . $this->id;
-    }
-
-    /**
      * Returns the field layout used by this element.
      *
      * @return FieldLayout|null
@@ -328,47 +155,9 @@ class Employee extends Element
         return null;
     }
 
-    public function getGroup()
-    {
-        if ($this->groupId === null) {
-            throw new InvalidConfigException('Tag is missing its group ID');
-        }
-
-        if (($group = Craft::$app->getTags()->getTagGroupById($this->groupId)) === null) {
-            throw new InvalidConfigException('Invalid tag group ID: '.$this->groupId);
-        }
-
-        return $group;
-    }
-
     // Indexes, etc.
     // -------------------------------------------------------------------------
 
-    /**
-     * Returns the HTML for the element’s editor HUD.
-     *
-     * @return string The HTML for the editor HUD
-     */
-    public function getEditorHtml(): string
-    {
-        $html = Craft::$app->getView()->renderTemplateMacro('_includes/forms', 'textField', [
-            [
-                'label' => Craft::t('app', 'Title'),
-                'siteId' => $this->siteId,
-                'id' => 'title',
-                'name' => 'title',
-                'value' => $this->title,
-                'errors' => $this->getErrors('title'),
-                'first' => true,
-                'autofocus' => true,
-                'required' => true
-            ]
-        ]);
-
-        $html .= parent::getEditorHtml();
-
-        return $html;
-    }
 
     /**
      * @inheritdoc
@@ -397,18 +186,6 @@ class Employee extends Element
     // -------------------------------------------------------------------------
 
     /**
-     * Performs actions before an element is saved.
-     *
-     * @param bool $isNew Whether the element is brand new
-     *
-     * @return bool Whether the element should be saved
-     */
-    public function beforeSave(bool $isNew): bool
-    {
-        return true;
-    }
-
-    /**
      * Performs actions after an element is saved.
      *
      * @param bool $isNew Whether the element is brand new
@@ -417,11 +194,6 @@ class Employee extends Element
      */
     public function afterSave(bool $isNew)
     {
-//        if (!$this->propagating) {
-//
-//            $this->_saveRecord($isNew);
-//        }
-
         return parent::afterSave($isNew);
     }
 
@@ -442,8 +214,14 @@ class Employee extends Element
      */
     public function afterDelete()
     {
+        return true;
     }
 
+    /**
+     * Returns all employee ID's
+     *
+     * @return array
+     */
     private static function _getEmployeeIds(): array
     {
         $employeeIds = [];
