bf4de7fbe06e08dcf77c83704f18ebce567b4cbc
Stefanie Gevaert <>
2022-03-21 15:11:35 +0100

    refactor: totals getter as array instead of active record

--- src/Staff.php
+++ src/Staff.php
@@ -186,6 +186,8 @@ class Staff extends Plugin
 
         $this->installEventListeners();
 
+//        Craft::dd(Staff::$plugin->payRuns->getTotalsById(1));
+
         /**
          * Logging in Craft involves using one of the following methods:
          *
