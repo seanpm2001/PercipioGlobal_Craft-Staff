df3cfe6b225f73cd4d1d042ab433b0feeaad8a8f
Michael Thomas <michael@dadamotion.be>
2022-03-21 17:39:19 +0100

    feat(gql): authorization token fetch

--- src/web/assets/src/js/composables/useApolloClient.ts
+++ src/web/assets/src/js/composables/useApolloClient.ts
@@ -1,11 +1,24 @@
 import { ApolloClient, from, InMemoryCache, HttpLink } from '@apollo/client/core'
 import { onError } from '@apollo/client/link/error'
-import { useAxiosClient } from '~/js/composables/useAxiosClient'
+import { setContext } from '@apollo/client/link/context'
+import { getToken } from '~/js/composables/useAxiosClient'
+
+
 
 // HTTP connection to the API
 const httpLink = new HttpLink({
     uri: '',
     credentials: 'include'
+})
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from the store, if it's null fetch it through axios
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Authorization: Bearer ${token}` : null
+        }
+    }
 })
 
 // Handle errors
