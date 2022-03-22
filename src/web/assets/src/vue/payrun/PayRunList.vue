57dd3e2a9cda02d513cb6bdef715c93907604ec2
Michael Thomas <michael@dadamotion.be>
2022-03-22 16:37:00 +0100

    feat(copy): updated copy

--- src/web/assets/src/vue/payrun/PayRunList.vue
+++ src/web/assets/src/vue/payrun/PayRunList.vue
@@ -45,7 +45,7 @@
                 class="cursor-pointer inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 disabled:bg-indigo-400 disabled:cursor-not-allowed px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto" 
                 style="margin-bottom:0"
             >
-                <span>Fetch Pay Runs</span>
+                <span>Refresh Pay Runs</span>
                 <svg v-if="store.loadingFetched" class="animate-spin ml-1 h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" style="margin-bottom:0">
                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
