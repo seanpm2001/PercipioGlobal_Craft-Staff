0bc1678fd80a5ef7288179176810475ee4834d9d
Michael Thomas <michael@dadamotion.be>
2022-03-22 21:15:49 +0100

    fix(css): alignment issue

--- src/web/assets/src/vue/molecules/listitems/listitem--employer.vue
+++ src/web/assets/src/vue/molecules/listitems/listitem--employer.vue
@@ -31,7 +31,7 @@
         :title="`Go to pay runs of ${employer.name}`" 
         class="grid grid-cols-6 border-b border-solid border-gray-200 no-underline hover:bg-gray-200" 
     >
-        <div class="col-span-2 whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-indigo-800 sm:pl-6 flex">
+        <div class="flex items-center col-span-2 whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-indigo-800 sm:pl-6">
             <div class="object-cover object-center w-6 h-6 rounded-full overflow-hidden mb-0">
                 <img 
                     :src="employer.logoUrl" 
@@ -40,9 +40,9 @@
             </div>
             <span style="margin-bottom:0">{{ employer.name }}</span>
         </div>
-        <div class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.crn ? employer.crn : '-' }}</div>
-        <div class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.employeeCount }}</div>
-        <div class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.currentPayRun?.taxYear }} / {{ employer.currentPayRun?.period }}</div>
-        <div class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.dateSynced }}</div>
+        <div class="flex items-center whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.crn ? employer.crn : '-' }}</div>
+        <div class="flex items-center whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.employeeCount }}</div>
+        <div class="flex items-center whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.currentPayRun?.taxYear }} / {{ employer.currentPayRun?.period }}</div>
+        <div class="flex items-center whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.dateSynced }}</div>
     </a>
 </template>
