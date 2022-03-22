91f624a4b6cf5b46b611d49be30b1e365e004c1c
Michael Thomas <michael@dadamotion.be>
2022-03-22 16:18:58 +0100

    refactor(gql): cleaned up store

--- src/web/assets/src/js/stores/payrun.ts
+++ src/web/assets/src/js/stores/payrun.ts
@@ -4,10 +4,8 @@ export const usePayRunStore = defineStore('payrun', {
     id: 'payrun',
 
     state: () => ({
-        employeeCount: 115,
         queue: 0,
-        loadingFetched: false,
-        token: null,
+        fetching: false,
         logs: []
     }),
 })
