4a2ed048be67052a47cb9941f0f07b11541604d1
Michael Thomas <michael@dadamotion.be>
2022-03-22 17:59:30 +0100

    fix(build): removed ext

--- src/web/assets/src/js/payruns.ts
+++ src/web/assets/src/js/payruns.ts
@@ -2,7 +2,7 @@ import PayRuns from '~/vue/payrun/PayRunList.vue'
 import { createApp, h, provide } from 'vue'
 import { createPinia } from 'pinia'
 import { DefaultApolloClient } from '@vue/apollo-composable'
-import { defaultClient } from '~/js/composables/useApolloClient.ts'
+import { defaultClient } from '~/js/composables/useApolloClient'
 
 
 const payruns = async () => {
