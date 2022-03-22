0c0e252fafe6e6e2834cb035cf83fc3413ffea53
Michael Thomas <michael@dadamotion.be>
2022-03-22 15:08:16 +0100

    feat(dates): added dateTimeAttributes to payrun

--- src/web/assets/src/vue/molecules/listitems/listitem--employer.vue
+++ src/web/assets/src/vue/molecules/listitems/listitem--employer.vue
@@ -5,20 +5,21 @@
 </script>
 
 <template>
+    {{ employer }}
     <a 
         :href="`/admin/staff-management/pay-runs/${employer.id}`" 
         :title="`Go to pay runs of ${employer.name}`" 
         class="grid grid-cols-6 border-b border-solid border-gray-200" 
     >
         <div class="col-span-2 whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-indigo-800 sm:pl-6 flex">
-            <div class="object-cover object-center w-6 h-6 rounded-full overflow-hidden" style="margin-bottom:0px">
+            <div class="object-cover object-center w-6 h-6 rounded-full overflow-hidden mb-0">
                 <img :src="employer.logoUrl" class="w-full h-full" />
             </div>
-            <span style="margin-bottom:0px">{{ employer.name }}</span>
+            <span style="margin-bottom:0">{{ employer.name }}</span>
         </div>
         <div class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.crn }}</div>
         <div class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.employeeCount }}</div>
-        <div class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.currentPayRun }}</div>
+        <div class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.currentYear }}</div>
         <div class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ employer.synced }}</div>
     </a>
 </template>
