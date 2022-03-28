18bc4eb3baf70c66d89ac75ea1c76dd079805afe
Stefanie Gevaert <>
2022-03-28 12:04:09 +0200

    feat(vue): added tax year to fetch pay run

--- src/web/assets/src/vue/payrun/PayRunList.vue
+++ src/web/assets/src/vue/payrun/PayRunList.vue
@@ -49,7 +49,7 @@
         <div class="mt-4 md:mt-0 flex" style="margin-bottom:0">
             <StatusSynced :date="getLatestSync(result.payruns)" />
             <button 
-                @click="fetchPayRuns(result.payruns[0]?.employerId)" 
+                @click="fetchPayRuns(result.payruns[0]?.employerId, currentYear)" 
                 :disabled="store.loadingFetched" 
                 class="cursor-pointer inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 disabled:bg-indigo-400 disabled:cursor-not-allowed px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto" 
                 style="margin-bottom:0"
