cf57e4f4effdd31f55c1a57b215c144fcbf72175
Stefanie Gevaert <>
2022-09-15 16:31:06 +0200

    feat: updated the fetch template to get the fetches from the CP panel

--- src/web/assets/src/js/composables/useAxiosClient.ts
+++ src/web/assets/src/js/composables/useAxiosClient.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { usePayRunStore } from '~/stores/payrun'
+import { useFetchStore } from '~/stores/fetch'
 
 const ENDPOINT = window.api.cpUrl ?? 'https://localhost:8003'
 
@@ -36,20 +37,6 @@ export const fetchPayRun = (payRunId: string) => {
 
 }
 
-export const getQueue = () => {
-
-    const store = usePayRunStore()
-
-    axios({
-        method: 'get',
-        url: `${ENDPOINT}/admin/staff-management/pay-runs/queue`,
-    })
-    .then((response) => {
-        store.queue = response?.data?.total ? response.data.total : 0
-    })
-
-}
-
 export const getPayRunLogs = (payRunId: string) => {
 
     const store = usePayRunStore()
@@ -61,6 +48,76 @@ export const getPayRunLogs = (payRunId: string) => {
         .then((response) => {
             store.logs = response?.data?.logs ? response.data.logs : []
             // store.loadingFetched = false
+        })
+
+}
+
+export const fetchDataEmployers = () => {
+    const store = useFetchStore()
+
+    store.fetchingEmployers = true
+
+    axios({
+        method: 'get',
+        url: `${ENDPOINT}/staff-management/fetch/employers`,
+    })
+        .then((response) => {
+            store.fetchingEmployers = false
+        })
+}
+
+export const fetchDataEmployees = () => {
+    const store = useFetchStore()
+
+    store.fetchingEmployees = true
+
+    axios({
+        method: 'get',
+        url: `${ENDPOINT}/staff-management/fetch/employees`,
+    })
+        .then((response) => {
+            store.fetchingEmployees = false
+        })
+}
+
+export const fetchDataPayRuns = () => {
+    const store = useFetchStore()
+
+    store.fetchingPayRun = true
+
+    axios({
+        method: 'get',
+        url: `${ENDPOINT}/staff-management/fetch/pay-runs`,
+    })
+        .then((response) => {
+            store.fetchingPayRun = false
+        })
+}
+
+export const fetchDataPayRunEntries = () => {
+    const store = useFetchStore()
+
+    store.fetchingPayRunEntries = true
+
+    axios({
+        method: 'get',
+        url: `${ENDPOINT}/staff-management/fetch/pay-run-entries`,
+    })
+        .then((response) => {
+            store.fetchingPayRunEntries = false
+        })
+}
+
+export const getQueue = () => {
+
+    const store = usePayRunStore()
+
+    axios({
+        method: 'get',
+        url: `${ENDPOINT}/admin/staff-management/pay-runs/queue`,
+    })
+        .then((response) => {
+            store.queue = response?.data?.total ? response.data.total : 0
         })
 
 }
